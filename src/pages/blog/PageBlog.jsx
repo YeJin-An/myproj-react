c0781ef82a42742db826fa86cf7e9f1449f103d9
안예진 <ahw24033@naver.com>
2022-01-10 16:11:52 +0900

    블로그 페이비 레이아웃 만들기

--- src/pages/blog/PageBlog.jsx
+++ src/pages/blog/PageBlog.jsx
@@ -1,7 +1,7 @@
 function PageBlog() {
   return (
     <div class="container mx-auto px-4">
-      <h2>PageBlog</h2>
+      <h2 className="text-red-400 border-b-2 border-red-300">PageBlog</h2>
     </div>
   );
 }
